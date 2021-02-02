2bf4c0f55ed2338d96ff55bc941da2507977c13e
kamilwolanski <kwolanski3@gmail.com>
2021-02-02 15:44:35 +0100

    final

--- 02/studdybuddy-front/src/components/StudentsList/StudentsList.js
+++ 02/studdybuddy-front/src/components/StudentsList/StudentsList.js
@@ -1,30 +1,36 @@
-import React, { useState } from 'react';
-import StudentProfile from 'components/StudentProfile/StudentProfile';
-import { Wrapper } from 'components/SchoolNews/SchoolNews.styles';
+import React, { useEffect, useState } from "react";
+import StudentProfile from "components/StudentProfile/StudentProfile";
+import { Wrapper } from "components/SchoolNews/SchoolNews.styles";
+import { api, endpoints } from 'api'
 
 const StudentsList = () => {
-  const [students, setStudents] = useState([
-    {
-      name: 'Fake',
-      age: 32,
-      group: 'Fake Group',
-      grades: [2,5,4,4,5,5],
+  const [students, setStudents] = useState([]);
+
+  async function fetchData() {
+    try {
+      const response = await api
+        .get(endpoints.users)
+        .then((response) => response.data);
+      setStudents(response);
+    } catch (err) {
+      console.log(err);
     }
-  ]);
+  }
+
+  useEffect(() => {
+      fetchData();
+  }, []);
 
   return (
     <Wrapper>
-      <StudentProfile studentData={students[0]}/>
-      <StudentProfile studentData={students[0]}/>
-      <StudentProfile studentData={students[0]}/>
-      <StudentProfile studentData={students[0]}/>
-      <StudentProfile studentData={students[0]}/>
-      <StudentProfile studentData={students[0]}/>
-      <StudentProfile studentData={students[0]}/>
-      <StudentProfile studentData={students[0]}/>
+      {students.length ? (
+        students.map(student => <StudentProfile key={student._id} studentData={student}/>)
+      ) : (
+        <h2>'Not students avaible'</h2>
+      )}
     </Wrapper>
   );
-}
+};
 
 StudentsList.propTypes = {};
 
